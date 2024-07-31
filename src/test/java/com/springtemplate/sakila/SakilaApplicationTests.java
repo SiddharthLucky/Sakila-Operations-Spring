b048d87a24130f992d26307cfb668fc555671499
SiddharthLucky <siddharth.luckyreddy@gmail.com>
2024-07-31 16:39:51 +0530

    initial commit

---
+++ src/test/java/com/springtemplate/sakila/SakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtemplate.sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
